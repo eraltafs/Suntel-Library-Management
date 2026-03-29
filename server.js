720456cfbfbda10459c0c16bb0d91cbc6ede4525
Altaf Khan <teacheraltafs@gmail.com>
2026-03-29 08:53:29 +0530

    intialized mongoose connection and server

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 import express from "express";
 import cors from "cors";
+import { connection } from "./config/db.js";
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, async () => {
+    try {
+        await connection()
+        console.log("listening on port", PORT)
+
+    } catch (error) {
+        console.error("error while listening", error.message)
+    }
 });
